d80783bbc4d40154881d79534320f7dfa821c918
akash <akash@HP>
2022-05-24 22:24:40 +0530

    hey

---
+++ src/main/java/Testscript/Test2.java
@@ -0,0 +1,12 @@
+package Testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void tc3() {
+		Reporter.log("java programing langauge",true);
+	}
+
+}
---
+++ src/main/java/Testscript/Test3.java
@@ -0,0 +1,11 @@
+package Testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+    @Test
+    public void tc4() {
+    	Reporter.log("Html scripted language",true);
+    }
+}
